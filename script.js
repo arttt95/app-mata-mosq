7d4545633ee01d07e224a2ab8269d0dbfa818c70
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-08-04 03:23:06 -0300

    04AGO24 - Aula 231 - App Game Mata Mosquito (Definição do palco do jogo)

---
+++ script.js
@@ -0,0 +1,17 @@
+var altura = 0
+var largura = 0
+
+function ajustaTamanhoPalcoJogo() {
+
+    //altura
+    altura = window.innerHeight
+
+    //largura
+    largura = window.innerWidth
+
+    console.log(altura, largura)
+
+}
+
+ajustaTamanhoPalcoJogo()
+
