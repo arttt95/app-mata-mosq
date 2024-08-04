ad5a9ec35124afbd41c4dac763501f8468bc689d
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-08-04 05:14:37 -0300

    04AGO24 - Aula 235 - App Game Mata Mosquito (Criando e removendo mosquitos)

--- script.js
+++ script.js
@@ -17,6 +17,11 @@ ajustaTamanhoPalcoJogo()
 
 function posicaoRandomica () {
 
+    //remover o mosquito anterior (caso exista)
+    if (document.getElementById('mosca')) {
+        document.getElementById('mosca').remove()
+    }
+
     var posicaoX = Math.floor(Math.random() * largura) - 90
     var posicaoY  = Math.floor(Math.random() * altura) - 90
 
@@ -32,10 +37,9 @@ function posicaoRandomica () {
     mosca.style.left = posicaoX + 'px'
     mosca.style.top = posicaoY + 'px'
     mosca.style.position = 'absolute'
+    mosca.id = 'mosca'
 
     document.body.appendChild(mosca)
-
-    console.log(ladoAleatorio())
 
 }
 
